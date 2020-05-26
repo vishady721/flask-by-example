8a17ff6bf134fa91f95256ca3e0d807d6ff298b2
Vishruti Ganesh <vishruti721@gmail.com>
2020-05-26 05:36:20 -0700

    Add functionality

--- app.py
+++ app.py
@@ -1,18 +1,34 @@
-from flask import Flask
-import os
+from flask import Flask, render_template, jsonify, request
+import geomatching
+import date_to_string
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
-print(os.environ['APP_SETTINGS'])
+
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def main():
+    return render_template('dateChooser.html')
+
+@app.route('/', methods=["POST"])
+def postMethod():
+    dateString = request.form["date"]
+    relevantRequesters = getRequestersAfterDate(date_to_string.date_to_num(dateString))
+    print("Date to Number {}".format(date_to_string.date_to_num(dateString)))
+    print("Relevant Requesters" +str(relevantRequesters))
+    nameList = [dict["NAME"] for dict in relevantRequesters.values()]
+    print(nameList)
+    return render_template('requestersChecklist.html', ids= nameList)
+
+#@app.route('/1', methods=["GET"])
+#def postMethod():
+ #   return
 
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+def getRequestersAfterDate(date):
+    volunteer_dict, request_dict = geomatching.main()
+    newDict = {key: value for key, value in request_dict.items() if key > date}
+    return newDict
 
 if __name__ == '__main__':
     app.run()
+
