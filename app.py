0e0bb3a93f068aac3cd9c85e4df61ec803b9beb0
Vishruti Ganesh <vishruti721@gmail.com>
2020-05-26 08:44:31 -0700

    updates

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ def getRequestersAfterDate(date):
 @app.route('/1', methods=["GET"])
 def functest():
     ids = [u'ids']
+    print(ids)
     return render_template('afterchoosing.html', ids1=ids)
 
 if __name__ == '__main__':
